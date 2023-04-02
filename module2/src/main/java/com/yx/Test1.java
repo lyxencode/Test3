6fe13498fa5cb74be47d009e6f5329406697414f
lyxencode <919663644@qq.com>
2023-04-02 12:11:20 +0800

    second commit

---
+++ module2/src/main/java/com/yx/Test1.java
@@ -0,0 +1,10 @@
+package com.yx;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("111111111");
+        System.out.println("222222222");
+    }
+
+}
