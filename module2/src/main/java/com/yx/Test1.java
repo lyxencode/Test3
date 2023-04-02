21ef7d8501768cc4afba56ad070731bdb64c72bf
lyxencode <919663644@qq.com>
2023-04-02 13:59:41 +0800

    hot-fix test first commit

--- module2/src/main/java/com/yx/Test1.java
+++ module2/src/main/java/com/yx/Test1.java
@@ -7,6 +7,7 @@ public class Test1 {
         System.out.println("222222222");
         System.out.println("333333333");
         System.out.println("444444444");
+        System.out.println("hot-fix test");
     }
 
 }
